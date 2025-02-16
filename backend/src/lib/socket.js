f0cb7d8a207730599f0cdfdedae9cbd820c4f55f
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-02-16 16:59:48 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,44 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
 import { Server } from "socket.io";
 import http from "http";
-import cors from "cors";
 import express from "express";
-
 const app = express();
 const server = http.createServer(app);
-
-app.use(cors());
 
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
-    credentials: true,
   },
 });
 
-const userSocketMap = {};
-
-// Function to get the socket ID of a receiver
-export function getReceiverSocketId(userId) {
-  return userSocketMap[userId] || null; 
-}
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-  const userId = socket.handshake.query.userId;
-  if (userId) {
-    userSocketMap[userId] = socket.id;
-  }
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    
-    if (userId) {
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    }
+@@ -33,4 +33,4 @@
   });
 });
 

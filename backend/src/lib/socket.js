0dbb65d255353f261fea45c79a4430d508028519
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-02-16 15:55:31 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,38 +4,41 @@ import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
 import express from "express";
+
 const app = express();
 const server = http.createServer(app);
+
 app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: ["synkr.vercel.app"],
-     methods: ["GET", "POST"],
+    origin: ["https://synkr.vercel.app"],
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
+const userSocketMap = {};
+
+// Function to get the socket ID of a receiver
 export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
+  return userSocketMap[userId] || null; 
 }
-
-
-const userSocketMap = {}; 
-
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
-
-
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    
+    if (userId) {
+      delete userSocketMap[userId];
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    }
   });
 });
 
